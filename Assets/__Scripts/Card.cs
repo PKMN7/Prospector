f603b7cdc425bb20b9efe93da95ab913ce12cbf3
PKMN7 <JL@nbp-236-80.nbp.ruw.rutgers.edu>
2016-04-07 00:18:33 -0400

    Almost
    Almost there… Gameplay works, but some visual oddities

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -2,43 +2,102 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Card : MonoBehaviour {
-
-	public string    suit;
-	public int       rank;
-	public Color     color = Color.black;
-	public string    colS = "Black";  // or "Red"
+public class Card : MonoBehaviour
+{
+	public string suit; 
+	public int rank; 
+	public Color color = Color.black; 
+	public string colS = "Black"; 
 	
 	public List<GameObject> decoGOs = new List<GameObject>();
 	public List<GameObject> pipGOs = new List<GameObject>();
 	
-	public GameObject back;     // back of card;
-	public CardDefinition def;  // from DeckXML.xml		
-
+	public GameObject back; 
 	
-	// property
-	public bool faceUP {
-		get {
+	public CardDefinition def; 
+	public SpriteRenderer[] spriteRenderers;
+	
+	void Start()
+	{
+		SetSortOrder(0);  
+	}
+	
+	public void PopulateSpriteRenderers()
+	{
+		if (spriteRenderers == null || spriteRenderers.Length == 0)
+		{
+			spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		}
+	}
+	
+	public void SetSortingLayerName(string tSLN)
+	{
+		PopulateSpriteRenderers();
+		
+		foreach (SpriteRenderer tSR in spriteRenderers)
+		{
+			tSR.sortingLayerName = tSLN;
+		}
+	}
+	
+	public void SetSortOrder(int sOrd)
+	{
+		PopulateSpriteRenderers();
+		
+		foreach (SpriteRenderer tSR in spriteRenderers)
+		{
+			if (tSR.gameObject == this.gameObject)
+			{
+				tSR.sortingOrder = sOrd;  
+				continue; 
+				
+			}
+			switch (tSR.gameObject.name)
+			{
+			case "back": 
+				tSR.sortingOrder = sOrd + 2;
+				break;
+			case "Card_Front": 
+				tSR.sortingOrder = 0;
+				break;
+			default:   
+				tSR.sortingOrder = sOrd + 1;
+				break;
+			}
+		}
+	}
+	
+	virtual public void OnMouseUpAsButton()
+	{
+		print(name);  
+	}
+	
+	public bool faceUp
+	{
+		get
+		{
 			return (!back.activeSelf);
-		}		
-		set {
+		}
+		set
+		{
 			back.SetActive(!value);
 		}
-	}	
-} // class Card
-
-[System.Serializable]
-public class Decorator{
-	public string	type;			// For card pips, tyhpe = "pip"
-	public Vector3	loc;			// location of sprite on the card
-	public bool		flip = false;	//whether to flip vertically
-	public float 	scale = 1.0f;
+	}
 }
 
 [System.Serializable]
-public class CardDefinition{
-	public string	face;	//sprite to use for face cart
-	public int		rank;	// value from 1-13 (Ace-King)
-	public List<Decorator>	
-					pips = new List<Decorator>();  // Pips Used					
-} // Class CardDefinition
+public class Decorator
+{
+	public string type; 
+	public Vector3 loc; 
+	public bool flip = false;
+	public float scale = 1f;    
+}
+
+[System.Serializable]
+public class CardDefinition
+{
+	public string face; 
+	public int rank;
+	public List<Decorator> pips = new List<Decorator>(); 
+}
